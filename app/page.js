ad3de80aff5a822090b32d38339cb41dddd1b60d
Govind Naag <govindnaag44@gmail.com>
2025-07-17 20:45:08 +0530

    add dark

--- app/page.js
+++ app/page.js
@@ -10,26 +10,26 @@ import { useEffect, useState } from "react";
  
 export default function Home() {
 
-  const [isDarkMode,setIsDarkMode]=useState(false)
-  useEffect(()=>{
-    if(localStorage.theme=== 'dark' || (!('theme' in localStorage) && 
-  matchMedia('(prefers-color-scheme:dark)').matches)){
-    setIsDarkMode(true)
-    }
-    else{
-      setIsDarkMode(false)
-    }
-  },[])
-  useEffect(()=>{
-    if(isDarkMode){
-      document.documentElement.classList.add('dark')
-      localStorage.theme = 'dark'
-    }
-    else{
-      document.documentElement.classList.remove('dark')
-      localStorage.theme = ''
-    }
-  },[isDarkMode])
+  const [isDarkMode,setIsDarkMode]=useState(true)
+  // useEffect(()=>{
+  //   if(localStorage.theme=== 'dark' || (!('theme' in localStorage) && 
+  // matchMedia('(prefers-color-scheme:dark)').matches)){
+  //   setIsDarkMode(true)
+  //   }
+  //   else{
+  //     setIsDarkMode(false)
+  //   }
+  // },[])
+  // useEffect(()=>{
+  //   if(isDarkMode){
+  //     document.documentElement.classList.add('dark')
+  //     localStorage.theme = 'dark'
+  //   }
+  //   else{
+  //     document.documentElement.classList.remove('dark')
+  //     localStorage.theme = ''
+  //   }
+  // },[isDarkMode])
   return (
      <>
      <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
